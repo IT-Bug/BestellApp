95f6c8909658e9f9874f5dd0721c34077d003e6c
IT-Bug <Kevin-Kirchmeer@gmx.de>
2025-06-01 01:29:42 +0200

    Menu HTML, CSS added

---
+++ script.js
@@ -0,0 +1,16 @@
+let soups = [
+    {
+        headline: "",
+        imgDish: "",
+        name: "Pilmeni",
+        price: 12.99,
+        description: "Teigtaschen mit auswählbarer Füllung (Rindfleisch, Pute oder Schweinefleisch)",
+    },
+    {
+        headline: "",
+        imgDish: "",
+        name: "Borsch",
+        price: 7.50,
+        description: "Suppe Russischer Art mit Rindfleisch, Kraut, Rote Bete, KArotte, Kartoffel",
+    },
+];
