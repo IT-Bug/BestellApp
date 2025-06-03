f935ca9d627bc81f129b5716cea4b89edd065630
IT-Bug <Kevin-Kirchmeer@gmx.de>
2025-06-03 20:39:48 +0200

    db.js added with dishes

--- JS/template.js
+++
@@ -1,4 +0,0 @@
-
-function getTemplateMenu() {
-    let dishBoxRef = document.getElementById('dishBox');
-}
--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-let soups = [
-    {
-        headline: "",
-        imgDish: "",
-        name: "Pilmeni",
-        price: 12.99,
-        description: "Teigtaschen mit auswählbarer Füllung (Rindfleisch, Pute oder Schweinefleisch)",
-    },
-    {
-        headline: "",
-        imgDish: "",
-        name: "Borsch",
-        price: 7.50,
-        description: "Suppe Russischer Art mit Rindfleisch, Kraut, Rote Bete, KArotte, Kartoffel",
-    },
-];
+
+function getTemplateMenu() {
+    let dishBoxRef = document.getElementById('dishBox');
+}
