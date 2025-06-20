c169ffcfdfeee9f9988196fb5c281646e685e9ad
IT-Bug <Kevin-Kirchmeer@gmx.de>
2025-06-21 01:01:40 +0200

    update popUp window toggle function finished

--- JS/mobileBasket.js
+++ JS/mobileBasket.js
@@ -2,9 +2,17 @@
 function openBasketPopUp(){
     let popUpBasketRef = document.getElementById('popUpBasket');
     popUpBasketRef.innerHTML= "";
-    
+    popUpBasketRef.classList.toggle("display-none");
+
+
     popUpBasketRef.innerHTML= `
-        <div class="popUpBasket">Hallo</div>
+        <div class="popUpBasket">
+            <div id="basket" class="basket">
+              <img src="../assets/logos/basket.png" height="50px" alt="warenkorb logo"/>
+              <h2>Vergiss nicht dein Warenkorb zu füllen</h2>
+              <p>Suche deine Lieblingsgerichte aus oder probier etwas Neues, pack die Gerichte in dein Warenkorb und bestelle dein Essen.</p>
+            </div>
+        </div>
 
     `;
 
